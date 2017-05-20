9067d52432098911e4e5d9df17081feb001b3400
CrazyPassion <von.king2010@gmail.com>
2017-05-20 10:54:28 +0800

    update the project

--- cmake-build-debug/CMakeFiles/LearnCpp.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/LearnCpp.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/Users/vonking/Documents/C++/code/learn/main.cpp" "/Users/vonking/Documents/C++/code/learn/cmake-build-debug/CMakeFiles/Learn.dir/main.cpp.o"
+  "/Users/vonking/Documents/C++/code/LearnCpp/main.cpp" "/Users/vonking/Documents/C++/code/LearnCpp/cmake-build-debug/CMakeFiles/LearnCpp.dir/main.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "AppleClang")
 
---
+++ cmake-build-debug/CMakeFiles/LearnCpp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LearnCpp.dir/main.cpp.o"
+  "LearnCpp.pdb"
+  "LearnCpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LearnCpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
