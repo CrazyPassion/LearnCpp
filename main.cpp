61b24de6e6139eb8e5788d35b27cee37175470b8
CrazyPassion <von.king2010@gmail.com>
2017-09-24 18:12:50 +0800

    add google test for project

--- main.cpp
+++ main.cpp
@@ -1,18 +1,11 @@
 
 #include <iostream>
-#include "Include/C13_1.hpp"
+#include "learncpp/Include/Person.hpp"
 
 
 int main()
 {
-    int i = 0;
-    int *pi = &i;
-    ++*pi;
-    cout<<i<<endl;
-    if (--*pi == 0)
-    {
-        cout<<"--pi "<<i<<endl;
-    }
-    cout<<"after"<<i<<endl;
+    Person A("Jay");
+    std::cout<<A.get_name()<<std::endl;
     return 0;
 }
---
+++ test/basic_tests/basic_test.cpp
@@ -0,0 +1,8 @@
+/* Created by vonking on 2017/9/11. Copyright (c) 2017 Steven Fung. All rights reserved. */
+
+#include "gtest/gtest.h"
+
+TEST(basic_test,test_eq)
+{
+    EXPECT_EQ(1, 0);
+}
